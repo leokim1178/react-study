e4077ddc9acbb824ca7eb9caddf683c8521c9538
leokim1178 <leo950906@gmail.com>
2026-01-14 15:04:15 +0900

    초기 설정 파일 살펴보기

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
+// 일관된 코드 스타일과 품질 유지를 위해 ESLint 설정을 정의합니다.
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -20,4 +21,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+// vite 설정 파일
+// 플러그인 설정 등 다양한 빌드 옵션을 여기서 지정할 수 있습니다.
 export default defineConfig({
   plugins: [react()],
-})
+});
